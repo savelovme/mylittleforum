74d16680af32528794a1178611eb0866f024b823
savelovme <savelov.m@gmail.com>
2021-11-14 15:02:28 +0300

    first commit

---
+++ services/web/project/config.py
@@ -0,0 +1,11 @@
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    STATIC_FOLDER = f"{os.getenv('APP_FOLDER')}/project/templates"
+    MEDIA_FOLDER = f"{os.getenv('APP_FOLDER')}/project/media"
